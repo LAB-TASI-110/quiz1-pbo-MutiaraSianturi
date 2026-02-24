35b2a6a8a973115d6aaad58059d8ab60ee97a746
MutiaraSianturi <mutiarasianturi006@gmail.com>
2026-02-24 13:21:08 +0700

    Driver2.java
    QUIZ 1 PBO

--- src/driver/Driver2.java
+++ src/driver/Driver2.java
@@ -1,3 +1,40 @@
+// Nama : Mutiara Y.H. Sianturi
+// NIM  : 12S24045
+
+
+import java.util.Scanner;
+
 public class Driver2 {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah data (N): ");
+        int N = input.nextInt();
+
+        int[] nilai = new int[N];
+
+        System.out.println("Masukkan deret nilai:");
+        for (int i = 0; i < N; i++) {
+            nilai[i] = input.nextInt();
+        }
+
+        System.out.print("Masukkan jumlah kelompok: ");
+        int jumlahKelompok = input.nextInt();
+
+        System.out.print("Masukkan kode kelompok yang ingin dijumlahkan: ");
+        int kodeKelompok = input.nextInt();
+
+        int total = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (i % jumlahKelompok == kodeKelompok - 1) {
+                total += nilai[i];
+            }
+        }
+
+        System.out.println("Total nilai kelompok " + kodeKelompok + " adalah: " + total);
+
+        input.close();
+    }
 }
